9b1203a854794a932c354067dd8ed4a3936bb3c8
Winson Yang <winsonfz.yang@gmail.com>
2020-04-17 18:18:49 -0500

    tidy publications

---
+++ content/publications/_index.md
@@ -0,0 +1,8 @@
+# Here's a list of my publications
+
+## Link to publication 1
+askldjalksjds
+
+
+## Link to publication 2
+askdljalsdj
--- scripts/init_kickstart.sh
+++ scripts/init_kickstart.sh
@@ -26,7 +26,7 @@ rsync -av --exclude gallery/ ../themes/academic/exampleSite/content/home/ ../con
 
 # Install indices
 rsync -av ../themes/academic/exampleSite/content/post/_index.md ../content/post/_index.md
-rsync -av ../themes/academic/exampleSite/content/publication/_index.md ../content/publication/_index.md
+rsync -av ../themes/academic/exampleSite/content/publications/_index.md ../content/publications/_index.md
 rsync -av ../themes/academic/exampleSite/content/talk/_index.md ../content/talk/_index.md
 
 # Skip static dir - do not import the demo's media library
